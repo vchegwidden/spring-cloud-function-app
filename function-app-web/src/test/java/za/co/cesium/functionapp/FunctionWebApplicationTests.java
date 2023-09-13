8afebfda00890cc5e54121baf1c222dfe6643aaf
vincent <vchegwidden@gmail.com>
2023-09-13 12:11:04 +0200

    Add integration tests for function web app

--- function-app-web/src/test/java/za/co/cesium/functionapp/FunctionWebApplicationTests.java
+++ function-app-web/src/test/java/za/co/cesium/functionapp/FunctionWebApplicationTests.java
@@ -1,13 +1,55 @@
 package za.co.cesium.functionapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.net.URI;
 
-@SpringBootTest
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * Integration test for the HTTP server wrapping this application
+ */
+@FunctionalSpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class FunctionWebApplicationTests {
+
+	@Autowired
+	private TestRestTemplate rest;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testUpperCaseString() throws Exception {
+		ResponseEntity<String> result = this.rest.exchange(
+			RequestEntity.post(new URI("/upperCaseString")).body("hello"), String.class);
+		Assert.assertEquals("HELLO", result.getBody());
+	}
+
+	@Test
+	public void testUpperCaseStringWithFilterNotRude() throws Exception {
+		ResponseEntity<String> result = this.rest.exchange(
+			RequestEntity.post(new URI("/upperCaseWithFilter")).body("hello"), String.class);
+		Assert.assertEquals("[\"HELLO\"]", result.getBody());
+	}
+
+	@Test
+	public void testUpperCaseStringWithFilterIsRude() throws Exception {
+		ResponseEntity<String> result = this.rest.exchange(
+			RequestEntity.post(new URI("/upperCaseWithFilter")).body("rude"), String.class);
+		Assert.assertEquals("[]", result.getBody());
+	}
+
+	@Test
+	public void testReverseString() throws Exception {
+		ResponseEntity<String> result = this.rest.exchange(
+			RequestEntity.post(new URI("/reverseString")).body("hello"), String.class);
+		Assert.assertEquals("olleh", result.getBody());
 	}
 
 }
